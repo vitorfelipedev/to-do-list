008525384edfddba00e47b9acb1f04f9862140db
vitor-felipee <vitor07.felipe@gmail.com>
2026-03-13 09:40:33 -0300

    feat(ui): implement interactive status and category filters

---
+++ src/components/filters.js
@@ -0,0 +1,46 @@
+import { getTasks } from '../services/storage.js';
+import { createTaskList } from './taskList.js';
+
+export function createFilters() {
+  const events = ['click', 'touchstart'];
+  const filterStatus = document.querySelectorAll('.filter-status .filter-btn');
+  const filterCategory = document.querySelectorAll(
+    '.filter-category .filter-btn',
+  );
+
+  let currentStatus = 'all';
+  let currentCategory = 'all';
+
+  function renderList() {
+    const tasks = getTasks();
+    createTaskList(tasks, currentStatus, currentCategory);
+  }
+
+  filterStatus.forEach((btn) => {
+    events.forEach((event) => {
+      btn.addEventListener(event, () => {
+        removeActive(filterStatus);
+        btn.classList.add('active');
+        currentStatus = btn.dataset.filter;
+        renderList();
+      });
+    });
+  });
+
+  filterCategory.forEach((btn) => {
+    events.forEach((event) => {
+      btn.addEventListener(event, () => {
+        removeActive(filterCategory);
+        btn.classList.add('active');
+        currentCategory = btn.dataset.category;
+        renderList();
+      });
+    });
+  });
+
+  renderList();
+}
+
+function removeActive(buttons) {
+  buttons.forEach((btn) => btn.classList.remove('active'));
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+import { createFilters } from './components/filters.js';
 import Theme from './utils/theme.js';
 
 const theme = new Theme('.btn-toggle-theme');
 theme.init();
+
+createFilters();
